f0a4202dba45c421d4af8b15a916b4e0626d1cc6
AnnoyingRains <annoyingrain5@gmail.com>
2025-12-07 15:48:20 +1100

    add library.json, add optional Colour -> CRGB (FASTLED) casting, fix pragma pack, pass packet by reference instead of by value

--- src/libTailSync.h
+++ src/libTailSync.h
@@ -1,15 +1,22 @@
 #pragma once
-#include <cmath>
-#include <cstdint>
 
-#pragma pack(1)
+#ifdef LIBTAILSYNC_USE_FASTLED
+#include <FastLED.h>
+#endif
+
+// https://github.com/clangd/clangd/issues/1167
+static_assert(true, "I am here to end the preamble before the pragma!");
+
+#pragma pack(push, 1)
 struct Colour {
   uint8_t red = 0;
   uint8_t green = 0;
   uint8_t blue = 0;
+#ifdef LIBTAILSYNC_USE_FASTLED
+  operator CRGB() const { return CRGB{red, green, blue}; }
+#endif
 };
 
-#pragma pack(1)
 struct PacketHeader {
   uint8_t magic[2]; // Should be ASCII "TS"
   uint8_t version_type;
@@ -19,7 +26,6 @@ struct PacketHeader {
   uint8_t gettype() const;
 };
 
-#pragma pack(1)
 struct ColourPacket {
   Colour head[6] = {};
   Colour body[6] = {};
@@ -28,13 +34,13 @@ struct ColourPacket {
   Colour tail[6] = {};
 };
 
-#pragma pack(1)
 struct MetaPacket {
   uint8_t channelName[32] = {};
   uint8_t djName[32] = {};
   uint8_t songName[32] = {};
   uint8_t colourRate = 25; // assume 25 colour packets per second
 };
+#pragma pack(pop)
 
 struct Channel {
   uint8_t name[32] = {0};
@@ -45,8 +51,8 @@ extern uint8_t lastNonce;
 
 typedef void (*handleEndSession)();
 typedef void (*handlePulse)();
-typedef void (*handleColour)(ColourPacket packet);
-typedef void (*handleMetaChange)(MetaPacket packet);
+typedef void (*handleColour)(const ColourPacket &packet);
+typedef void (*handleMetaChange)(const MetaPacket &packet);
 
 void setColourCallback(handleColour);
 void setMetaChangeCallback(handleMetaChange);
